993379202944a8847d31d4c238d679c0484cc070
Tavares <lucas.tavares1996@hotmail.com>
2019-12-12 15:47:01 -0300

    Criação da get_absolute_url, metodo get_category_count e melhorando o template

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,14 @@
+from django.db.models import Count
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
+
+
+def get_category_count():
+    # method to see the name and the count of every category
+    queryset = Post.objects.values(
+        'categories__title').annotate(Count('categories__title'))
+    return queryset
 
 
 def index(request):
@@ -20,7 +28,15 @@ def index(request):
 
 
 def blog(request):
+    category_count = get_category_count()
+
+    # Grab all posts and orderby negative timestamp
     post_list = Post.objects.all().order_by('-timestamp')
+
+    # latest posts
+    most_recent = Post.objects.order_by('-timestamp')[0:3]
+
+    # Pagination
     paginator = Paginator(post_list, 8)
     page_request = 'page'
     page = request.GET.get(page_request)
@@ -33,11 +49,13 @@ def blog(request):
 
     context = {
         'queryset': paginated_queryset,
-        'page_request': page_request
+        'page_request': page_request,
+        'most_recent': most_recent,
+        'category_count': category_count
     }
 
     return render(request, 'blog.html', context)
 
 
-def post(request):
+def post(request, id):
     return render(request, 'post.html', {})
