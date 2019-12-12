43d8f85422fbaaf5523c402b7659734e32d3e223
Tavares <lucas.tavares1996@hotmail.com>
2019-12-12 13:28:07 -0300

    Passando as informações para a página blog.html

--- src/posts/views.py
+++ src/posts/views.py
@@ -3,17 +3,30 @@ from .models import Post
 
 
 def index(request):
-    queryset = Post.objects.filter(featured=True)
+    """
+        Grab only the posts that have featured=True, in future this will be my owl carousel
+    """
+    featured = Post.objects.filter(featured=True)
+    # Grab onny the first 3 posts and the latest, because of negative timestamp
+    latest = Post.objects.order_by('-timestamp')[0:3]
 
     context = {
-        'object_list': queryset
+        'object_list': featured,
+        'latest': latest
     }
 
     return render(request, 'index.html', context)
 
 
 def blog(request):
-    return render(request, 'blog.html', {})
+    #post_list = Post.objects.all()
+    post_list = Post.objects.all().order_by('-timestamp')
+
+    context = {
+        'post_list': post_list
+    }
+
+    return render(request, 'blog.html', context)
 
 
 def post(request):
