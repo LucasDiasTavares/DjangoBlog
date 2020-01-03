146a72e1bc060835dfceeb332763759fe89d5100
Tavares <lucas.tavares1996@hotmail.com>
2020-01-03 13:42:18 -0300

    Estética do form comentario

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -1,2 +1,19 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
+
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Digite aqui o seu comentario!',
+            'id': 'usercomment',
+            'name': 'usercomment',
+            'cols': '100',
+            'rows': '10',
+        })
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('content',)
